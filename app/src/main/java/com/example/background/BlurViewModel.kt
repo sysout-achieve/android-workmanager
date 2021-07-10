93669650bb3a5158a419a8dc391ca981a8cc9f43
hur.geon <hur.geon@meshkorea.net>
2021-07-10 17:17:58 +0900

    WorkManager 도입 WorkManager를 이용하여 이미지 blur처리 활용

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -19,6 +19,11 @@ package com.example.background
 import android.app.Application
 import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.background.workers.BlurWorker
 
 
 class BlurViewModel(application: Application) : AndroidViewModel(application) {
@@ -26,11 +31,15 @@ class BlurViewModel(application: Application) : AndroidViewModel(application) {
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
 
-    /**
-     * Create the WorkRequest to apply the blur and save the resulting image
-     * @param blurLevel The amount to blur the image
-     */
-    internal fun applyBlur(blurLevel: Int) {}
+    private val workManager = WorkManager.getInstance(application)
+
+    internal fun applyBlur(blurLevel: Int) {
+        val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
+            .setInputData(createInputDataForUri())
+            .build()
+
+        workManager.enqueue(blurRequest)
+    }
 
     private fun uriOrNull(uriString: String?): Uri? {
         return if (!uriString.isNullOrEmpty()) {
@@ -40,9 +49,14 @@ class BlurViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    /**
-     * Setters
-     */
+    private fun createInputDataForUri(): Data {
+        val builder = Data.Builder()
+        imageUri?.let {
+            builder.putString(KEY_IMAGE_URI, imageUri.toString())
+        }
+        return builder.build()
+    }
+
     internal fun setImageUri(uri: String?) {
         imageUri = uriOrNull(uri)
     }
